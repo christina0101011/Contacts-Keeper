032ca19b1df32906230dc788a422d350344ef58f
Christina Kapiy <xkapiy@gmail.com>
2018-01-02 21:46:47 +0200

    Updated design and functionality

--- Contacts_App/app.js
+++ Contacts_App/app.js
@@ -3,19 +3,28 @@
   $scope.currentPersonId = -1;
   $scope.contactsKeeper = [
     {
-      name: 'Christina',
+      firstName: 'Christina',
+      lastName: 'Kapiy',
       telephone: '0123456789',
       email: 'xkapiy@gmail.com'
+    },
+      {
+      firstName: 'Mark',
+      lastName: 'Ok',
+      telephone: '0123456789',
+      email: 'myspace@gmail.com'
     }
   ];
   $scope.addContact = function() {
-    if ( $scope.name != '' ) {
+    if ( $scope.firstName != '' ) {
       $scope.contactsKeeper.push({
-        name: $scope.name,
+        firstName: $scope.firstName,
+        lastName: $scope.lastName,
         telephone: $scope.telephone,
         email: $scope.email
       });
-      $scope.name = '';
+      $scope.firstName = '';
+      $scope.lastName = '';
       $scope.telephone = '';
       $scope.email = '';
     }
@@ -23,9 +32,12 @@
   $scope.saveContact = function() {
     if( $scope.currentContactId > -1 ){
       var id = $scope.currentContactId;
-      $scope.contactsKeeper[id].name = $scope.name; $scope.contactsKeeper[id].telephone = $scope.telephone;
+      $scope.contactsKeeper[id].firstName = $scope.firstName; 
+      $scope.contactsKeeper[id].lastName = $scope.lastName; 
+      $scope.contactsKeeper[id].telephone = $scope.telephone;
       $scope.contactsKeeper[id].email = $scope.email;
-      $scope.name = '';
+      $scope.firstName = '';
+      $scope.lastName = '';
       $scope.telephone = '';
       $scope.email = '';
       $scope.currentContactId = -1;
@@ -33,11 +45,13 @@
   }
   $scope.editContact = function ( id ) {
     $scope.currentPersonId = id;
-    $scope.name = $scope.contactsKeeper[id].name;
+    $scope.firstName = $scope.contactsKeeper[id].firstName;
+    $scope.lastName =
+    $scope.contactsKeeper[id].lastName;
     $scope.telephone = $scope.contactsKeeper[id].telephone;
     $scope.email = $scope.contactsKeeper[id].email;
   }
-  $scope.deleteContact = function( id ) {
+    $scope.deleteContact = function( id ) {
     $scope.contactsKeeper.splice( id, 1 );
   }
 })
